69412f4c3bffea46ca82d8c7861019315841226e
lambdadroid <lambdadroid@gmail.com>
2017-06-07 20:31:34 +0200

    Minor power management improvements

--- board/kernel.mk
+++ board/kernel.mk
@@ -4,7 +4,7 @@ TARGET_KERNEL_ARCH := x86_64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := x86_64-linux-android-
 BOARD_KERNEL_IMAGE_NAME := bzImage
 
-BOARD_KERNEL_CMDLINE += androidboot.hardware=me176c
+BOARD_KERNEL_CMDLINE += androidboot.hardware=me176c nmi_watchdog=0
 
 TARGET_USES_64_BIT_BINDER := true
 
--- product/init.mk
+++ product/init.mk
@@ -4,6 +4,7 @@ PRODUCT_PACKAGES += \
     init.me176c.usb.rc \
     init.battery.rc \
     init.thermal.rc \
+    init.power.rc \
     init.wifi.rc \
     ueventd.me176c.rc \
     fstab.recovery.me176c \
