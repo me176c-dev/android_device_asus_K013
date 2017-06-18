2541c25a921e5ec2bf949f28c884d4c67f1d3206
lambdadroid <lambdadroid@gmail.com>
2017-06-18 21:28:06 +0200

    Switch to INT340x thermal drivers and optimize thermald config

--- product/thermal.mk
+++ product/thermal.mk
@@ -2,6 +2,8 @@ PRODUCT_PACKAGES += \
     thermald
 
 THERMALD_GENERATE_CONFIG := false
+THERMALD_DETECT_THERMAL_ZONES := false
+THERMALD_BACKLIGHT_COOLING_DEVICE := false
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-conf.xml:system/etc/thermald/thermal-conf.xml
