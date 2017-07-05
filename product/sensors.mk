7cf4f18507e177884c1590c4dd1b8026c88de16b
lambdadroid <lambdadroid@gmail.com>
2017-07-05 22:07:04 +0200

    Add modified version of tablet_core_hardware.xml
    Don't pretend we support Bluetooth + Microphone when we don't (yet).

--- product/sensors.mk
+++ product/sensors.mk
@@ -6,7 +6,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.iio.accel.panel=5 \
     ro.iio.accel.rotation=4 \
     ro.iio.magn.quirks=no-trig
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml
