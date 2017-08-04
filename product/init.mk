b020fb073a5b5dd3df0cda73ce6790352d206fbf
lambdadroid <lambdadroid@gmail.com>
2017-08-04 16:52:38 +0200

    Fix permission denied for /factory partition on TWRP. Fixes #21

--- product/init.mk
+++ product/init.mk
@@ -2,7 +2,6 @@ PRODUCT_PACKAGES += \
     fstab.me176c \
     init.me176c.rc \
     ueventd.me176c.rc \
-    fstab.recovery.me176c \
     twrp.fstab \
     init.recovery.me176c.rc \
     init.recovery.usb.rc
