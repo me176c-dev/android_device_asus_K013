8b67eb09577e6a8af459aa23ea72f10846160a54
lambdadroid <lambdadroid@gmail.com>
2017-08-07 16:10:42 +0200

    Set PRODUCT_BRAND to asus. Fixes #27

--- lineage.mk
+++ lineage.mk
@@ -2,8 +2,10 @@ $(call inherit-product, device/asus/me176c/device.mk)
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 PRODUCT_NAME := lineage_me176c
-PRODUCT_DEVICE := me176c
-PRODUCT_MODEL := ASUS MeMO Pad 7 (ME176C)
-PRODUCT_MANUFACTURER := asus
 
+PRODUCT_BRAND := asus
+PRODUCT_DEVICE := me176c
 TARGET_VENDOR_DEVICE_NAME := K013
+
+PRODUCT_MANUFACTURER := ASUS
+PRODUCT_MODEL := ASUS MeMO Pad 7 (ME176C)
