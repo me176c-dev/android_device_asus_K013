cf9d15cc60fc78632874d61d25020fd3717ff922
lambdadroid <lambdadroid@gmail.com>
2017-09-04 18:44:49 +0200

    product: bluetooth: Set correct Bluetooth MAC address from oemcfg

--- product/bluetooth.mk
+++ product/bluetooth.mk
@@ -1,2 +1,5 @@
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bt.bdaddr_path=/oemcfg/bt/bd_addr.conf
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
