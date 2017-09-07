1ca9bf767e6306bd0f7e49e4ce0d494fc2a33d1f
lambdadroid <lambdadroid@gmail.com>
2017-09-07 13:22:25 +0200

    board: twrp: Exclude TWRP app

--- board/twrp.mk
+++ board/twrp.mk
@@ -4,6 +4,7 @@ RECOVERY_SDCARD_ON_DATA := true
 TW_THEME := portrait_hdpi
 TWRP_INCLUDE_CRYPTO := true
 TW_EXCLUDE_SUPERSU := true
+TW_EXCLUDE_TWRPAPP := true
 
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file"
