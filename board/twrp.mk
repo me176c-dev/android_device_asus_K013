e4796506aec8f68410ef6db5b60859c5360b7438
lambdadroid <lambdadroid@gmail.com>
2017-11-11 15:42:46 +0100

    board: twrp: Set correct path to CPU thermal zone

--- board/twrp.mk
+++ board/twrp.mk
@@ -9,6 +9,8 @@ TW_EXCLUDE_TWRPAPP := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file"
 
+TW_CUSTOM_CPU_TEMP_PATH := /sys/class/thermal/thermal_zone6/temp
+
 TW_NO_LEGACY_PROPS := true
 
 TARGET_RECOVERY_UPDATER_LIBS := libasus_updater
