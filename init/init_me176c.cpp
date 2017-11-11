a5b5894f6754eeb142ab658ee1ca6c81c69baf47
lambdadroid <lambdadroid@gmail.com>
2017-11-11 11:57:45 +0100

    Cleanup init_me176c.cpp and use more C++ features

--- init/init_me176c.cpp
+++ init/init_me176c.cpp
@@ -1,50 +1,44 @@
-#include <stdio.h>
-#include <errno.h>
-#include <string.h>
+#include <fstream>
+#include <cerrno>
+#include <cstring>
 
-#ifdef REPLACE_PROPERTIES
+#ifndef REPLACE_PROPERTIES
+#include "property_service.h"
+#else
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
-#define property_replace(name, value) \
+#define property_set(name, value) \
     __system_property_update((prop_info*) __system_property_find(name), value, strlen(value))
-#else
-#include "property_service.h"
-#define property_replace property_set
 #endif
 
 #include "log.h"
 
-#define PHONE_INFO "/factory/PhoneInfodisk/PhoneInfo_inf"
+#define PHONE_INFO  "/factory/PhoneInfodisk/PhoneInfo_inf"
 
 /* Serial number */
-#define SERIALNO_PROP "ro.serialno"
-#define SERIALNO_SIZE 12+1
+#define SERIALNO_PROP  "ro.serialno"
+#define SERIALNO_SIZE  12+1
 
 static void load_serialno() {
-    int ret;
-    char serial[SERIALNO_SIZE];
-
     // Open file
-    FILE *file = fopen(PHONE_INFO, "r");
+    std::ifstream file(PHONE_INFO);
     if (!file) {
         ERROR("Failed to open phone info file: %s\n", strerror(errno));
         return;
     }
 
-    // Read serial from file
-    if (!fgets(serial, SERIALNO_SIZE, file)) {
+    // Read serialno from file
+    char serialno[SERIALNO_SIZE];
+    if (!file.get(serialno, SERIALNO_SIZE)) {
         ERROR("Failed to read serialno from phone info file: %s\n", strerror(errno));
-        goto err;
+        return;
     }
 
-    // Set serial property
-    ret = property_replace(SERIALNO_PROP, serial);
+    // Set serialno property
+    int ret = property_set(SERIALNO_PROP, serialno);
     if (ret) {
-        ERROR("Failed to set %s property: %d\n", SERIALNO_PROP, ret);
+        ERROR("Failed to set " SERIALNO_PROP " property: %d\n", ret);
     }
-
-err:
-    fclose(file);
 }
 
 void vendor_load_properties() {
