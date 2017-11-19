e95a4710f9f4c979a021ec3c477ad2cc74bb1b4b
lambdadroid <lambdadroid@gmail.com>
2017-11-19 12:38:46 +0100

    Include OpenGApps public key in recovery

--- product/recovery.mk
+++ product/recovery.mk
@@ -1,4 +1,5 @@
 # Include Lambdadroid and LineageOS keys in recovery
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     $(LOCAL_PATH)/recovery/lambdadroid \
-    $(LOCAL_PATH)/recovery/lineage
+    $(LOCAL_PATH)/recovery/lineage \
+    $(LOCAL_PATH)/recovery/open_gapps
