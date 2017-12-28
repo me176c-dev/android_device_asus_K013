151e3e21207c877f0a831af8affb8b6545473d68
lambdadroid <lambdadroid@gmail.com>
2017-12-28 15:36:27 +0100

    device: Remove dalvik.gc.type-precise from PRODUCT_TAGS
    I don't know if this actually does anything, and various sources
    suggest that it's obsolete. So just drop it. Bye.

--- device.mk
+++ device.mk
@@ -2,7 +2,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
 
 PRODUCT_CHARACTERISTICS := tablet
-PRODUCT_TAGS += dalvik.gc.type-precise
 
 PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
