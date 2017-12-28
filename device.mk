7363f7dcfc87e4b14d1f41828995f69685f25334
lambdadroid <lambdadroid@gmail.com>
2017-12-28 15:38:36 +0100

    device: Set ro.product.first_api_level to 19 (Android 4.4 KitKat)

--- device.mk
+++ device.mk
@@ -5,6 +5,9 @@ PRODUCT_CHARACTERISTICS := tablet
 
 PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# Tablet originally shipped with Android 4.4 KitKat
+PRODUCT_SHIPPING_API_LEVEL := 19
+
 include $(LOCAL_PATH)/product/*.mk
 
 $(call inherit-product, vendor/asus/me176c/proprietary.mk)
