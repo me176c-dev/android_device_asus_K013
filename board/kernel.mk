953033773e97ad6e131a64c539d0baffff96123d
lambdadroid <lambdadroid@gmail.com>
2018-01-26 19:39:58 +0100

    board: kernel: Use host distribution compiler for Retpoline support

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,7 +1,14 @@
 TARGET_KERNEL_SOURCE := kernel/asus/me176c
 TARGET_KERNEL_DEFCONFIG = $(BUILT_ME176C_DEFCONFIG)
 TARGET_KERNEL_ARCH := x86_64
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := x86_64-linux-android-
+
+# Use host distribution compiler if it is recent enough for Retpoline support
+ifeq ($(shell printf "%s\n" "7.3" "`gcc -dumpversion`" | sort -cV 2>&1),)
+    TARGET_KERNEL_CROSS_COMPILE_PREFIX :=
+else
+    TARGET_KERNEL_CROSS_COMPILE_PREFIX := x86_64-linux-android-
+endif
+
 BOARD_KERNEL_IMAGE_NAME := bzImage
 
 BOARD_KERNEL_CMDLINE += quiet androidboot.hardware=me176c tsc=reliable rfkill.default_state=0
