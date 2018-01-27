7ef3e08b2da4214e254e9125b80a9567b0de29e4
lambdadroid <lambdadroid@gmail.com>
2018-01-27 17:21:19 +0100

    Move /oemcfg to /vnddata
    It doesn't really contain any "configuration" files, so rename it
    to vendor data to make its purpose more clear.

--- product/bluetooth.mk
+++ product/bluetooth.mk
@@ -1,5 +1,6 @@
+# TODO: Move to /factory
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.bt.bdaddr_path=/oemcfg/bt/bd_addr.conf
+    ro.bt.bdaddr_path=/vnddat/bt/bd_addr.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
