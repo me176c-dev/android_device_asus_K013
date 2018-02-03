d0286db9d1ccf7d0888d0dcfe707fe72c863bca7
lambdadroid <lambdadroid@gmail.com>
2018-02-03 13:45:36 +0100

    Revert "product: houdini: Avoid using ADDITIONAL_DEFAULT_PROPERTIES"
    Seems like this isn't working properly in Nougat.
    
    This reverts commit a2e758646652047f3c5075b50280ebe0b152e4c8.

--- product/houdini.mk
+++ product/houdini.mk
@@ -2,5 +2,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.dalvik.vm.isa.arm=x86 \
     ro.enable.native.bridge.exec=1
 
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.dalvik.vm.native.bridge=libhoudini.so
