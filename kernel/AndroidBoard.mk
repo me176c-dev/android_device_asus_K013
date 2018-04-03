8ca5d762fbb17001c41c718a96ce7fc5f802a64e
lambdadroid <lambdadroid@gmail.com>
2018-04-03 12:15:27 +0200

    kernel: Move proprietary parts of defconfig to vendor repo

--- kernel/AndroidBoard.mk
+++ kernel/AndroidBoard.mk
@@ -1,3 +1,3 @@
-LOCAL_PATH := $(call my-dir)
-include $(LOCAL_PATH)/defconfig.mk
-include $(LOCAL_PATH)/kernel.mk
+LOCAL_PATH_KERNEL := $(call my-dir)
+include $(LOCAL_PATH_KERNEL)/defconfig.mk
+include $(LOCAL_PATH_KERNEL)/kernel.mk
--- kernel/defconfig.mk
+++ kernel/defconfig.mk
@@ -4,10 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := me176c_defconfig
 LOCAL_MODULE_CLASS := FAKE
 LOCAL_SRC_FILE := me176c_defconfig
+include $(BUILD_PREBUILT)
 
-include $(BUILD_SYSTEM)/base_rules.mk
-$(LOCAL_BUILT_MODULE): $(LOCAL_PATH)/$(LOCAL_SRC_FILE) dsdt.hex
-	$(copy-file-to-target)
-	@echo CONFIG_ACPI_CUSTOM_DSDT_FILE=\"$(abspath $(BUILT_ACPI_DSDT_TABLE))\" >> $@
-
-BUILT_ME176C_DEFCONFIG := $(LOCAL_BUILT_MODULE)
+TARGET_KERNEL_DEFCONFIG := $(LOCAL_BUILT_MODULE)
+include vendor/asus/me176c/kernel_defconfig.mk
