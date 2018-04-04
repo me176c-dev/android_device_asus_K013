ce42d5443a38515e21e7d24035e5ce5f6b8cedb0
lambdadroid <lambdadroid@gmail.com>
2018-04-04 11:43:42 +0200

    kernel: Avoid using BUILD_PREBUILT
    Doesn't seem to update automatically, meh..

--- kernel/defconfig.mk
+++ kernel/defconfig.mk
@@ -4,7 +4,10 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := me176c_defconfig
 LOCAL_MODULE_CLASS := FAKE
 LOCAL_SRC_FILE := me176c_defconfig
-include $(BUILD_PREBUILT)
+
+include $(BUILD_SYSTEM)/base_rules.mk
+$(LOCAL_BUILT_MODULE): $(LOCAL_PATH)/$(LOCAL_SRC_FILE)
+	$(copy-file-to-target)
 
 TARGET_KERNEL_DEFCONFIG := $(LOCAL_BUILT_MODULE)
 include vendor/asus/me176c/kernel_defconfig.mk
