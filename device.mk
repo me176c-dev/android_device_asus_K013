541d921d0537e954e48b3c8b17bfd1d108f409f1
lambdadroid <lambdadroid@gmail.com>
2018-10-22 21:04:30 +0200

    Use binderized Bluetooth HAL

--- device.mk
+++ device.mk
@@ -97,7 +97,8 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbt-vendor \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
 
 # Hardware
 PRODUCT_COPY_FILES += \
