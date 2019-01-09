13048481d75357bfc973223cd7e6edf2a08c2ca6
lambdadroid <lambdadroid@gmail.com>
2019-01-09 20:10:17 +0100

    Use drmfb-composer and Chromium minigbm instead of FB HAL

--- device.mk
+++ device.mk
@@ -47,13 +47,12 @@ TARGET_SCREEN_HEIGHT := 800
 PRODUCT_PACKAGES += \
     libdrm \
     libdrm_intel \
-    gralloc.android_ia \
+    gralloc.minigbm \
     libGLES_mesa \
-    vulkan.mesa \
+    vulkan.anv \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.allocator@2.0-impl \
-    android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service.drmfb \
     android.hardware.graphics.mapper@2.0-impl
 
 # Audio
