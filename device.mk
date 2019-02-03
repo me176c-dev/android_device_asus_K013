69cd7412d043c715138b906e5ab3d34d121f06cc
lambdadroid <lambdadroid@gmail.com>
2019-02-03 11:40:56 +0100

    device: Enable Android Go build configuration defaults
    At this point, a tablet with 1 GB RAM can be regarded as a "low ram"
    device, as modern devices usually come with at least 4 GB RAM.
    The Android Go build configuration is specifically optimized for
    devices with 1 GB (or even less) RAM. So it makes sense to enable
    them for this tablet.
    
    It does come with a price: generally, less RAM is traded for more
    CPU usage. Some features like splitscreen get disabled. But all in
    all it might improve the situation a bit.

--- device.mk
+++ device.mk
@@ -1,4 +1,5 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/go_defaults.mk)
 $(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
 
 PRODUCT_BRAND := asus
