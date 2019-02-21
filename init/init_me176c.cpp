82b76a18ce81578fd97f20a06afd655bcfe481a7
lambdadroid <lambdadroid@gmail.com>
2019-02-21 14:17:25 +0100

    init: Use shared me176c-factory code, mount "factory" temporarily only

--- init/init_me176c.cpp
+++ init/init_me176c.cpp
@@ -1,6 +1,6 @@
-#include <fstream>
 #include <android-base/logging.h>
 #include <property_service.h>
+#include <me176c-factory.h>
 
 // ro.serialno is already set by init, so we need to hack a bit to replace it
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
@@ -10,67 +10,24 @@
 
 using android::init::property_set;
 
-#define PHONE_INFO_FILE  "/factory/PhoneInfodisk/PhoneInfo_inf"
+#define FACTORY_BLOCK_DEVICE  "/dev/block/by-name/factory"
+#define FACTORY_MOUNT_PATH    "/mnt/vendor/factory"
 
-// Serial number
 #define SERIALNO_PROP  "ro.serialno"
-#define SERIALNO_LEN   12
-
-// MAC address
-#define MAC_ADDR_LEN   17
-#define BDADDR_POS     0x99
 #define BDADDR_PROP    "ro.boot.btmacaddr"
-#define WIFI_MAC_POS   0xcc
-#define WIFI_MAC_PROP  "ro.boot.wifimacaddr"
-
-namespace {
-void load_serialno(std::ifstream& file) {
-    // Read serialno from file
-    char serialno[SERIALNO_LEN + 1];
-    serialno[SERIALNO_LEN] = 0;
-    if (!file.read(serialno, SERIALNO_LEN)) {
-        PLOG(ERROR) << "Failed to read serialno";
-        return;
-    }
-
-    // Replace serialno property
-    if (auto ret = property_replace(SERIALNO_PROP, serialno); ret) {
-        LOG(ERROR) << "Failed to set " SERIALNO_PROP " property: " << ret;
-        return;
-    }
-}
-
-void load_mac_addr(std::ifstream& file, std::streampos pos, const std::string& prop, std::string& addr) {
-    if (!file.seekg(pos)) {
-        PLOG(ERROR) << "Failed to seek to " << prop;
-        return;
-    }
-
-    for (auto i = 0; i < MAC_ADDR_LEN; i += 3) {
-        addr[i] = file.get();
-        addr[i+1] = file.get();
-    }
-
-    if (!file) {
-        PLOG(ERROR) << "Failed to read " << prop;
-        return;
-    }
-
-    property_set(prop, addr);
-}
-}
+#define WIFIADDR_PROP  "ro.boot.wifimacaddr"
 
 void vendor_load_properties() {
-    // Open file
-    std::ifstream file{PHONE_INFO_FILE, std::ifstream::binary};
-    if (!file) {
-        PLOG(ERROR) << "Failed to open phone info file (" PHONE_INFO_FILE ")";
+    struct me176c_factory_info info;
+    if (me176c_factory_load(FACTORY_BLOCK_DEVICE, FACTORY_MOUNT_PATH, &info)) {
+        PLOG(ERROR) << "Failed to load factory info from " FACTORY_BLOCK_DEVICE;
         return;
     }
 
-    load_serialno(file);
+    if (int ret = property_replace(SERIALNO_PROP, info.serialno); ret) {
+        LOG(ERROR) << "Failed to set " SERIALNO_PROP " property: " << ret;
+    }
 
-    std::string addr(MAC_ADDR_LEN, ':');
-    load_mac_addr(file, BDADDR_POS, BDADDR_PROP, addr);
-    load_mac_addr(file, WIFI_MAC_POS, WIFI_MAC_PROP, addr);
+    property_set(BDADDR_PROP, info.bdaddr);
+    property_set(WIFIADDR_PROP, info.wifiaddr);
 }
